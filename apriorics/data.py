7c974e7d517bcf71d8a87ceef883d7ad9ce1d343
Legrand Matthieu <legrand.ma@chu-toulouse.fr>
2022-02-07 11:25:38 +0100

    Implementing the patch dataset

--- apriorics/data.py
+++ apriorics/data.py
@@ -1,18 +1,29 @@
+from os import PathLike
+from typing import List, Tuple
 from torch.utils.data import Dataset
-from cucim import CuImage
+from pathaia.util.types import Slide 
 from pathaia.util.basic import ifnone
 from albumentations import Compose
+import csv
 
+
+SLIDES_BACKEND = "cucim"
+DEFAULT_PATCH_SIZE = (1024,1024)
 
 class SegmentationDataset(Dataset):
-    def __init__(self, slide_paths, mask_paths, patches, transforms=None):
+    def __init__(self, slide_paths:List[PathLike], mask_paths:List[PathLike], patches_paths:List[PathLike], patch_size:Tuple[int,int] = DEFAULT_PATCH_SIZE, transforms=None):
         super().__init__()
-        self.slides = [CuImage(str(slide_path)) for slide_path in slide_paths]
-        self.mask_rles = []
-        for mask_path in mask_paths:
-            with open(mask_path, "r") as f:
-                self.mask_rles.append(f.read().rstrip())
-        self.patches = patches
+        self.slides = [Slide(slide_path, backend=SLIDES_BACKEND) for slide_path in slide_paths]
+        self.masks = [Slide(mask_path, backend=SLIDES_BACKEND) for mask_path in mask_paths]
+        self.patches = []
+        self.patches_slide = []
+        for [slide_idx,patches_path] in enumerate(patches_paths):
+            with open(patches_path) as patch_file : 
+                reader = csv.DictReader(patch_file)
+                for patch in reader :
+                    self.patches.append(patch)
+                    self.patches_slide.append(slide_idx)
+        self.patch_size = patch_size
         self.transforms = Compose(ifnone(transforms, []))
 
     def __len__(self):
@@ -20,4 +31,6 @@ class SegmentationDataset(Dataset):
 
     def __getitem__(self, idx):
         patch = self.patches[idx]
-        slide = self.slides[self.slide_idxs[idx]]
+        slide = self.slides[self.patches_slide[idx]]
+        mask = self.masks[self.patches_slide[idx]]
+        return slide.read_region((patch["x"],patch["y"]), 0, self.patch_size), mask.read_region((patch["x"],patch["y"]), 0, self.patch_size)

c9c1212660326571bbb7e60059d0b0c793fbab95
SCHWOB Robin <schwob.ro@chu-toulouse.fr>
2022-10-05 14:16:10 +0200

    script to make slides readonly on AprioricsSlides mount

---
+++ readonly_slides.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+for i in {1..9}
+do
+        sudo chmod 555 /media/AprioricsSlides/*-${i}_*.svs
+done
+
+for i in {13..21}
+do
+        sudo chmod 555 /media/AprioricsSlides/*-${i}_*.svs
+done

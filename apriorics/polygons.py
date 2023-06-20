98bf3be845b46006f343206169d8ad7cab964dce
SCHWOB Robin <schwob.ro@chu-toulouse.fr>
2023-06-20 14:26:33 +0200

    hovernet to geojson now takes class predictions into account

--- apriorics/polygons.py
+++ apriorics/polygons.py
@@ -2,15 +2,14 @@ import json
 from os import PathLike
 from typing import Optional
 
-import geopandas
 import numpy as np
 from albumentations.augmentations.crops.functional import get_center_crop_coords
 from nptyping import Int, NDArray, Number, Shape
+from pathaia.util.basic import ifnone
 from pathaia.util.types import NDBoolMask
 from rasterio import features
-from shapely.affinity import affine_transform, translate
+from shapely.affinity import affine_transform
 from shapely.geometry import MultiPolygon, Polygon, shape
-from shapely.ops import clip_by_rect
 
 
 def get_reduced_coords(
@@ -155,22 +154,63 @@ def hovernet_to_geojson(
         outfile: path to output wkt file.
         crop_size: Size of the crop zone.
     """
+    type_info = {
+        0: ["none", [0, 0, 0]],
+        1: ["tumoral", [255, 0, 0]],
+        2: ["immunitaire", [0, 255, 0]],
+        3: ["conjonctif", [0, 0, 255]],
+        4: ["nécrose", [255, 255, 0]],
+        5: ["épithélial", [255, 165, 0]],
+    }
     with open(infile, "r") as f:
         hovernet_dict = json.load(f)
-    polygons = []
+    geojson_feats = []
+    xmin = 0
+    ymin = 0
+    xmax = 0
+    ymax = 0
     for k in hovernet_dict["nuc"]:
-        polygon = Polygon(hovernet_dict["nuc"][k]["contour"])
-        if polygon.is_valid:
-            polygons.append(polygon)
-    polygons = MultiPolygon(polygons)
+        nuc = hovernet_dict["nuc"][k]
+        coords = np.array(nuc["contour"])
+        bbox = np.array(nuc["bbox"])
+        offset = np.array([[ifnone(xoff, 0), ifnone(yoff, 0)]])
+        coords += offset
+        bbox += offset
+        xmin, ymin = np.minimum((xmin, ymin), bbox[0])
+        xmax, ymax = np.maximum((xmax, ymax), bbox[1])
+        label, color = type_info[nuc["type"]]
+        feat = {
+            "id": k,
+            "type": "Feature",
+            "properties": {
+                "objectType": "annotation",
+                "classification": {"name": label, "color": color},
+            },
+            "geometry": {
+                "type": "Polygon",
+                "coordinates": [coords.tolist() + [coords[0].tolist()]],
+                "bbox": bbox.flatten().tolist(),
+            },
+        }
+        geojson_feats.append(feat)
 
     if crop_size is not None:
-        xmin, ymin, xmax, ymax = polygons.bounds
         x0, y0, x1, y1 = get_center_crop_coords(
             ymax - ymin, xmax - xmin, crop_size, crop_size
         )
-        polygons = clip_by_rect(polygons, x0 + xmin, y0 + ymin, x1 + xmin, y1 + ymin)
-        polygons = translate(polygons, xoff=xoff, yoff=yoff)
+        xmin += x0
+        ymin += y0
+        xmax += x1
+        ymax += y1
+        geojson_feats = [
+            feat
+            for feat in geojson_feats
+            if feat["geometry"]["bbox"][0] >= xmin
+            and feat["geometry"]["bbox"][1] >= ymin
+            and feat["geometry"]["bbox"][2] <= xmax
+            and feat["geometry"]["bbox"][3] <= ymax
+        ]
 
+    geojson_dict = {"type": "FeatureCollection", "features": geojson_feats}
     with open(outfile, "w") as f:
-        json.dump(geopandas.GeoSeries(polygons.geoms).__geo_interface__, f)
+        json.dump(geojson_dict, f)
